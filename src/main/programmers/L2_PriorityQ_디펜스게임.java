1a6e7257a91a4ad7fc684cfe3cc4a323edb89c03
yeongbinim <yeim.dev@gmail.com>
2024-04-11 16:56:43 +0900

    PRG(L2): 디펜스게임(PriorityQ)

---
+++ src/main/programmers/L2_PriorityQ_디펜스게임.java
@@ -0,0 +1,30 @@
+package main.programmers;
+
+import java.util.*;
+
+public class L2_PriorityQ_디펜스게임 {
+    public int solution(int n, int k, int[] enemy) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        k = Math.min(k, enemy.length);
+
+        for (int i = 0; i < k; i++) {
+            pq.add(enemy[i]);
+        }
+
+        int round = k;
+        while (round < enemy.length) {
+            int prev = pq.poll();
+            int cur = enemy[round];
+            int damage = Math.min(prev, cur);
+            pq.add(Math.max(prev, cur));
+
+            n -= damage;
+            if (n < 0) {
+                break;
+            }
+            round++;
+        }
+
+        return round;
+    }
+}
