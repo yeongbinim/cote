e08153f87bbf3c96a86f80eb904b16f59da987d8
yeongbinim <yeim.dev@gmail.com>
2024-05-03 20:55:55 +0900

    PRG(L2): 2xN타일링(DP)

---
+++ src/main/programmers/L2_DP_2xN타일링.java
@@ -0,0 +1,16 @@
+package main.programmers;
+
+public class L2_DP_2xN타일링 {
+    private final int MOD = 1_000_000_007;
+    public int solution(int n) {
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
