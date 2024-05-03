78c2b8e93ec90dd2f53ee0932306d0bda5309e99
yeongbinim <yeim.dev@gmail.com>
2024-05-03 16:57:42 +0900

    PRG(L3): 거스름돈(DP)

---
+++ src/main/programmers/L3_DP_거스름돈.java
@@ -0,0 +1,23 @@
+package main.programmers;
+
+public class L3_DP_거스름돈 {
+    public int solution(int n, int[] money) {
+        long[][] dp = new long[money.length + 1][n + 1];
+
+        for (int r = 0; r < dp.length; r++) {
+            dp[r][0] = 1;
+        }
+
+        for (int r = 1; r < dp.length; r++) {
+            for (int c = 1; c < dp[r].length; c++) {
+                dp[r][c] = dp[r - 1][c];
+                if (c >= money[r - 1]) {
+                    dp[r][c] += dp[r][c - money[r - 1]];
+                    dp[r][c] %= 1_000_000_007;
+                }
+            }
+        }
+
+        return (int) dp[money.length][n];
+    }
+}
