a82193bb9cf23e1616413c69095459b768ebef12
yeongbinim <yeim.dev@gmail.com>
2024-05-08 15:08:07 +0900

    PRG(L2): 택배배달과수거하기(Greedy)

---
+++ src/main/programmers/L2_Greedy_택배배달과수거하기.java
@@ -0,0 +1,23 @@
+package main.programmers;
+
+public class L2_Greedy_택배배달과수거하기 {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long distSum = 0;
+
+        int moveCount = 0;
+        int deliverySum = 0;
+        int pickupSum = 0;
+
+        for (int dist = n; dist > 0; dist--) {
+            deliverySum += deliveries[dist - 1];
+            pickupSum += pickups[dist - 1];
+            int maxQuotient = (int) Math.max(Math.ceil(deliverySum / (double) cap), Math.ceil(pickupSum / (double) cap));
+            if (maxQuotient > moveCount) {
+                distSum += dist * (maxQuotient - moveCount) * 2L;
+                moveCount = maxQuotient;
+            }
+        }
+
+        return distSum;
+    }
+}
